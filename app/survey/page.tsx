ca303608e9208608ccc8b85c171d58d488fb9e46
Devon Wieczorek <devon.wieczorek93@gmail.com>
2025-12-27 18:48:25 -0500

    feat: implements survey functionality

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -5,13 +5,22 @@ import { useRouter } from 'next/navigation';
 import ProgressArea from "@/components/ProgressArea";
 import UserContext from "@/lib/contexts/UserContext";
 import ProgressContext from "@/lib/contexts/ProgressContext";
+import SurveyContext from "@/lib/contexts/SurveyContext";
+import SurveyQuestion from './components/SurveyQuestion';
 
 const { UserProvider, useUser } = UserContext;
 const { ProgressProvider } = ProgressContext;
+const { SurveyProvider, useSurvey } = SurveyContext;
 
 function SurveyContent() {
 	const router = useRouter();
 	const { userInfo } = useUser();
+	const {
+		questions,
+		currentQuestionIndex,
+		handleAnswer,
+		isLoading
+	} = useSurvey();
 
 	useEffect(() => {
 		// Redirect to home if no email is set (user hasn't completed step one)
@@ -26,10 +35,36 @@ function SurveyContent() {
 		return null;
 	}
 
+	if (isLoading) {
+		return (
+			<div className="w-[95%] max-w-[960px] mx-auto bg-white rounded min-h-[500px] md:min-h-[300px] pt-8 px-4">
+				<div className="max-width-600">
+					<p>Loading survey questions...</p>
+				</div>
+			</div>
+		);
+	}
+
+	const currentQuestion = questions[currentQuestionIndex];
+
+	if (!currentQuestion) {
+		return (
+			<div className="w-[95%] max-w-[960px] mx-auto bg-white rounded min-h-[500px] md:min-h-[300px] pt-8 px-4">
+				<div className="max-width-600">
+					<p>No more questions available.</p>
+				</div>
+			</div>
+		);
+	}
+
 	return (
-		<div className="page-wrapper bg-white rounded">
+		<div className="w-[95%] max-w-[960px] mx-auto bg-white rounded min-h-[500px] md:min-h-[300px] pt-8 px-4">
 			<div className="max-width-600">
-				Survey Content Here
+				<SurveyQuestion
+					surveyQuestion={currentQuestion}
+					questionIndex={currentQuestionIndex}
+					onAnswer={handleAnswer}
+				/>
 			</div>
 		</div>
 	);
@@ -39,8 +74,10 @@ export default function Survey() {
 	return (
 		<UserProvider>
 			<ProgressProvider initialStep={4}>
-				<ProgressArea />
-				<SurveyContent />
+				<SurveyProvider>
+					<ProgressArea />
+					<SurveyContent />
+				</SurveyProvider>
 			</ProgressProvider>
 		</UserProvider>
 	);
